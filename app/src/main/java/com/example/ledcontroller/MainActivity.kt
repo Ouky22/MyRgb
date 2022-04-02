30901c7937bb3319b66e65af334756de4673299f
Mike <menzelmike123@gmail.com>
2022-04-02 19:41:55 +0200

    implement toolbar

--- app/src/main/java/com/example/ledcontroller/MainActivity.kt
+++ app/src/main/java/com/example/ledcontroller/MainActivity.kt
@@ -6,26 +6,32 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.ledcontroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val host =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         binding.bottomNavView.setupWithNavController(host.navController)
 
         // set root destinations for which no up button should be displayed
-        val config = AppBarConfiguration(setOf(R.id.main_dest, R.id.alarm_dest, R.id.rgb_show_dest))
-        setupActionBarWithNavController(host.navController, config)
+        appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.main_dest, R.id.alarm_dest, R.id.rgb_show_dest))
+        setSupportActionBar(binding.topAppBar)
+        setupActionBarWithNavController(host.navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
