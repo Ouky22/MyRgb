4128dc5b6a41a978e592adfc269ec6ea8a60e116
Mike <menzelmike123@gmail.com>
2022-07-31 18:26:38 +0200

    toggle led strip status on button click

---
+++ app/src/main/java/com/example/ledcontroller/view/BindingAdapters.kt
@@ -0,0 +1,9 @@
+package com.example.ledcontroller.view
+
+import android.widget.Button
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("status_border")
+fun statusBorder(button: Button, activatedStatus: Boolean) {
+    // TODO set border of button accordingly to current status
+}
--- app/src/main/java/com/example/ledcontroller/viewmodel/ControllerViewModel.kt
+++ app/src/main/java/com/example/ledcontroller/viewmodel/ControllerViewModel.kt
@@ -16,11 +16,31 @@ class ControllerViewModel : ViewModel() {
     private val _currentlySelectedColor = MutableLiveData<RgbCircle.RgbTriplet>()
     val currentlySelectedColor = _currentlySelectedColor
 
+    private val _isSofaLedStripOn = MutableLiveData(false)
+    val isSofaLedStripOn = _isSofaLedStripOn
+    private val _isBedLedStripOn = MutableLiveData(false)
+    val isBedLedStripOn = _isBedLedStripOn
+    private val _isDeskLedStripOn = MutableLiveData(false)
+    val isDeskLedStripOn = _isDeskLedStripOn
+
 
     fun onRgbCircleTouch(touchPositionX: Int, touchPositionY: Int) {
         val angle = computeAngleBetweenTouchAndRgbCircleCenter(touchPositionX, touchPositionY)
         _currentlySelectedColor.value = rgbCircle.computeColorAtAngle(angle)
     }
+
+    fun onButtonSofaClick() {
+        _isSofaLedStripOn.value?.let { _isSofaLedStripOn.value = !it }
+    }
+
+    fun onButtonBedClick() {
+        _isBedLedStripOn.value?.let { _isBedLedStripOn.value = !it }
+    }
+
+    fun onButtonDeskClick() {
+        _isDeskLedStripOn.value?.let { _isDeskLedStripOn.value = !it }
+    }
+
 
     private fun computeAngleBetweenTouchAndRgbCircleCenter(x: Int, y: Int): Int {
         // This method computes the angle (0-359) between a vertical vector (0,-1) starting in the rgb
