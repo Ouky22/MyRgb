e269da368ceee8c194de3c25b1c860dc51635f3c
Mike <menzelmike123@gmail.com>
2022-07-31 23:03:41 +0200

    show activated status of strip buttons graphically

--- app/src/main/java/com/example/ledcontroller/view/BindingAdapters.kt
+++ app/src/main/java/com/example/ledcontroller/view/BindingAdapters.kt
@@ -1,9 +1,12 @@
 package com.example.ledcontroller.view
 
-import android.widget.Button
 import androidx.databinding.BindingAdapter
+import com.example.ledcontroller.R
 
 @BindingAdapter("status_border")
-fun statusBorder(button: Button, activatedStatus: Boolean) {
-    // TODO set border of button accordingly to current status
+fun statusBorder(button: com.google.android.material.button.MaterialButton, isActivated: Boolean) {
+    if (isActivated)
+        button.setStrokeColorResource(R.color.btn_color_on)
+    else
+        button.setStrokeColorResource(R.color.btn_color_off)
 }
