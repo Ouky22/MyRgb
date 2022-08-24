b7c651d5f7f6156ae3f85a848c55c10f4e0fd2d9
Mike <menzelmike123@gmail.com>
2022-08-24 12:52:56 +0200

    make getAllSortedByTriggerTime not suspend
    because it returns a Flow

--- app/src/main/java/com/example/ledcontroller/persistence/RgbAlarmDao.kt
+++ app/src/main/java/com/example/ledcontroller/persistence/RgbAlarmDao.kt
@@ -10,6 +10,9 @@ interface RgbAlarmDao {
     @Query("SELECT * FROM rgbAlarm ORDER BY trigger_time_minutes_of_day ASC")
     fun getAllSortedByTriggerTime(): Flow<List<RgbAlarm>>
 
+    @Query("SELECT * FROM rgbAlarm WHERE is_active = 1")
+    suspend fun getAllActiveAlarms(): List<RgbAlarm>
+
     @Query("SELECT * FROM rgbAlarm WHERE id = :id LIMIT 1")
     suspend fun getById(id: Int): RgbAlarm
 
--- app/src/main/java/com/example/ledcontroller/repository/RgbAlarmRepository.kt
+++ app/src/main/java/com/example/ledcontroller/repository/RgbAlarmRepository.kt
@@ -1,18 +1,22 @@
 package com.example.ledcontroller.repository
 
 import android.app.Application
+import androidx.compose.runtime.collectAsState
 import com.example.ledcontroller.App
 import com.example.ledcontroller.model.RgbAlarm
+import kotlinx.coroutines.flow.collect
 
 class RgbAlarmRepository(application: Application) {
 
     private val alarmDao = (application as App).ledControllerDatabase.rgbAlarmDao
 
     suspend fun getNextActivatedAlarm() {
-        // TODO
+        val activeAlarms = alarmDao.getAllActiveAlarms()
+        // TODO sort and get next alarm. Check if trigger date-time is not expired
+
     }
 
-    suspend fun getAllSortedByTriggerTime() = alarmDao.getAllSortedByTriggerTime()
+    fun getAllSortedByTriggerTime() = alarmDao.getAllSortedByTriggerTime()
 
     suspend fun getById(id: Int) = alarmDao.getById(id)
 
