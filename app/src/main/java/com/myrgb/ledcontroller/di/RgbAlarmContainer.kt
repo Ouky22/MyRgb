7174194ce9778b26f7fbb2c9955db5eb4bb8a165
Mike <menzelmike123@gmail.com>
2022-12-14 18:57:18 +0100

    remove RgbAlarmContainer
    because it is not used

--- app/src/main/java/com/myrgb/ledcontroller/di/RgbAlarmContainer.kt
+++
@@ -1,8 +0,0 @@
-package com.myrgb.ledcontroller.di
-
-import com.myrgb.ledcontroller.persistence.rgbalarm.RgbAlarmRepository
-import com.myrgb.ledcontroller.feature.rgbalarmclock.list.RgbAlarmListViewModel
-
-class RgbAlarmContainer(private val rgbAlarmRepository: RgbAlarmRepository) {
-    val rgbAlarmListViewModelFactory = RgbAlarmListViewModel.Factory(rgbAlarmRepository)
-}
